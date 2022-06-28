923935e817619bf2144231ddf4dc5198ddd2fb55
sergiopjunior <sergiohenriquemelges@hotmail.com>
2022-06-27 21:37:17 -0300

    Add typescript

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,15 +1,20 @@
-var result_input = document.getElementById("result-area");
+var result_input = document.getElementById("result-area") as HTMLInputElement;
 var old_input = "0";
-function validate_input(input) {
-    var accepted_values = /^[0-9/*+=-]+$/g;
-    return accepted_values.test(input);
+
+function validate_input(input: string) {
+    let accepted_values = /^[0-9/*+=-]+$/g;
+
+    return accepted_values.test(input)
 }
-function process_input(new_input) {
-    try {
+
+function process_input(new_input: string) {
+    try { 
         new_input = new_input.toString().toLowerCase();
+
         if (old_input.length < new_input.length) {
-            var last_char = new_input.charAt(new_input.length - 1);
+            let last_char = new_input.charAt(new_input.length - 1);
             console.log("Old: %s\nNew: %s\nLast Char: %s", old_input, new_input, last_char);
+
             if (validate_input(new_input)) {
                 console.log("Valid Input");
                 old_input = new_input;
@@ -17,44 +22,47 @@ function process_input(new_input) {
             }
             else {
                 result_input.value = old_input;
-            }
+            }       
         }
         else {
             old_input = new_input;
             result_input.value = old_input;
         }
     }
-    catch (e) {
+    catch(e) {
         console.log(e);
     }
 }
-function set_number(num) {
+
+function set_number(num: string) {
     try {
-        var text = result_input.value;
+        let text = result_input.value;
+
         if (num == ".") {
             if (text.indexOf('.') < 0) {
                 result_input.value += num;
-            }
+            }   
         }
         else if (text == "0") {
-            if (num != "0")
-                result_input.value = num;
-        }
-        else
-            result_input.value += num;
+            if (num != "0") result_input.value = num;
+        }  
+        else result_input.value += num;
     }
-    catch (e) {
+    catch(e) {
         console.log(e);
     }
 }
-function set_operator(op) {
+
+function set_operator(op: string) {
     result_input.value += op;
 }
+
 function clear_entry() {
     result_input.value = "0";
 }
-function calc() {
-    var result = eval(result_input.value);
+
+function calc(){
+    let result = eval(result_input.value);
     console.log(result);
     result_input.value = result;
 }
